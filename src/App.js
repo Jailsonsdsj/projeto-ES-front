4ca2266c6d58f852ded97a50bf0a6b26e9b203cb
Wytoria Faustino <86625354+wytoriaa@users.noreply.github.com>
2022-09-11 17:33:04 -0300

    login page and header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
 import Login from './components/loginForm/index';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import HeaderTest from './components/loginForm/header';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={ <Login />} />
+        <Route path='test' element={<HeaderTest/>}/>
       </Routes>
     </Router>
   );
